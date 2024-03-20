3ef0f64a0a93f9b57b680264775e4dd00fcbf203
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-03-20 11:32:35 +0300

    adds various functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (list) => list.slice(0, 2)
+const returnLastTwoDrivers = (list) => list.slice(-2)
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(numb) {
+    return function(numb){
+        return numb * 5
+    }
+}
+
+const fareDoubler = (numb) => numb * 2
+const fareTripler = (numb) => numb * 3
+
+const selectDifferentDrivers = function(list, selectedFunction) {
+    if (selectedFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(list)
+    }
+    else return returnLastTwoDrivers(list)
+}
